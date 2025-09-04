c53d9851a4225c903758a2a360fd3dbbc71c58e2
facherotqda <martinraularrua@gmail.com>
2025-09-04 17:06:32 -0300

    Primer respaldo del proyecto

---
+++ NEW2/src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, AlertController } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, CommonModule, FormsModule]
+})
+export class HomePage implements OnInit {
+
+  usuario: string = '';
+  contrasena: string = '';
+
+  constructor(private alertCtrl: AlertController) { }
+
+  ngOnInit() { }
+
+  async ingresar() {
+    const alert = await this.alertCtrl.create({
+      header: 'Datos ingresados',
+      message: `Usuario: ${this.usuario}<br>Contraseña: ${this.contrasena}`,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+}
